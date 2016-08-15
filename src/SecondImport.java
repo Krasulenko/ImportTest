9fc30e84cdf2a6753082a979573707e5ba4045ea
Krasulenko <krasul@ukr.net>
2016-08-16 01:17:03 +0300

    test of import

---
+++ src/SecondImport.java
@@ -0,0 +1,6 @@
+
+public class SecondImport {
+    public static void main(String[] args) {
+        System.out.println("Hello Everybody");
+    }
+}
---
+++ src/ThirdImport.java
@@ -0,0 +1,6 @@
+
+public class ThirdImport {
+    public static void main(String[] args) {
+        System.out.println("Hello ...");
+    }
+}
